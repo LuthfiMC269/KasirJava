510c545519a9fbc2716d6dd35a37939df17e932a
LuthfiMC269 <luhtfim269@gmail.com>
2024-10-13 15:45:39 +0700

    Mindahin Inisialisasi dari main ke Class Etalase

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
 import java.util.ArrayList; // Buat bikin Array
 import java.util.InputMismatchException; // Buat bikin input error detect
 import java.util.Scanner; // Buat Input
+
 public class Main {
-    // function print list barang
+    // function print list barang sesuai struktur list di lsBarang
     public static void list(ArrayList<lsBarang> menu){
        System.out.println("Menu:");
        for (lsBarang i : menu) {
            System.out.println(i.nama + " - Harga Rp." + i.harga);
        }
     }
+    // Main
     public static void main(String[] args) {
-        ArrayList<lsBarang> menu = new ArrayList<>();
-        menu.add(new lsBarang("1. Milo", 6000, 0));
-        menu.add(new lsBarang("2. Ayam Goreng", 8000, 0));
-        list(menu);
+        Etalase.InisialisasiEtalase();
+        list(Etalase.InisialisasiEtalase());
     }
 }
